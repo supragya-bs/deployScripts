94510032f2891f647bb2f85f6606d1a782f8c9b7
Supragya Raj <supragya@browserstack.com>
2020-02-24 13:53:54 +0530

    init get_browsers.py

---
+++ get_browsers.py
@@ -0,0 +1,15 @@
+import os
+
+def download_using_wget:
+    print "==> Downloading using WGET"
+
+def s3cmd_upload:
+    print "==> Uploading to s3 using s3cmd"
+
+def s3cmd_ls
+    print "==> s3cmd LS"
+
+if __name__ == "__main__":
+    print "=> GET BROWSERS"
+    print "==> Step 1"
+    download_using_wget
