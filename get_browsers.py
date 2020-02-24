e9985c0d61d2605994b7d322a855fe19c5779038
Supragya Raj <supragya@browserstack.com>
2020-02-24 14:20:36 +0530

    half working

--- get_browsers.py
+++ get_browsers.py
@@ -1,15 +1,41 @@
 import os
 
-def download_using_wget():
+# Windows: 
+
+dld = [
+    {
+        "Name": "Windows - Edge 80 stable",
+        "Url": "https://www.google.com",
+        "S3path": "http://s3.amazonaws.com/bs-platform/windows/browsers/edge/81.0.410.1_edge_installer.exe"
+    }
+]
+
+def download_using_wget(dct):
     print "==> Downloading using WGET"
+    print dct["Url"]
+    os.system("wget "+dct["Url"]+" -O ~/supragya/wget_temp/wgetdownload")
 
-def s3cmd_upload():
+def s3cmd_upload(dct):
     print "==> Uploading to s3 using s3cmd"
+    print dct["S3path"]
 
-def s3cmd_ls():
+def s3cmd_ls(dct):
     print "==> s3cmd LS"
 
 if __name__ == "__main__":
     print "=> GET BROWSERS"
-    print "==> Step 1"
-    download_using_wget
+    for b in dld:
+        print "-------------------------------------------------------------"
+        print b["Name"]
+        choice = input("Type 1 to proceed: ")
+        if choice != 1:
+            print "Ending"
+            exit()
+        print "==> Step 1"
+        download_using_wget(b)
+        print "==> Step 2"
+        s3cmd_upload(b)
+        print "==> Step 3"
+        s3cmd_ls(b)
+        print "-------------------------------------------------------------"
+
